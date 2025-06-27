55cc5dc12fa70c96d4678c46e66219bbd20a1e69
ahmadghader <63785169+ahmadghadercp@users.noreply.github.com>
2025-06-27 15:12:13 +0300

    Update user_controller.py

--- server/controllers/user_controller.py
+++ server/controllers/user_controller.py
@@ -1,6 +1,7 @@
-from models.user_model import SignupData
-from utils.auth import hash_password, verify_password, create_token, decode_token
-from database import users_collection
+from ..models.user_model import SignupData
+from ..utils.auth import hash_password, verify_password, create_token, decode_token
+from ..database import users_collection
+
 from bson import ObjectId
 from fastapi import HTTPException
 from jose import JWTError
